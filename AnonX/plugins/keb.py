c73830bac492c4f49515e4cd30721200fb144833
Almortagel <105941198+Almortage@users.noreply.github.com>
2023-09-03 03:29:57 +0300

    Add files via upload

--- AnonX/plugins/keb.py
+++ AnonX/plugins/keb.py
@@ -80,7 +80,7 @@ REPLY_MESSAGE_BUTTONS = [
     ]
 ]
 
-@app.on_message(filters.regex("^/almortagel"))
+@app.on_message(filters.regex("/almortagel"))
 async def cpanel(_, message: Message):             
         text = REPLY_MESSAGE
         reply_markup = ReplyKeyboardMarkup(REPLY_MESSAGE_BUTTONS, resize_keyboard=True, selective=True)
