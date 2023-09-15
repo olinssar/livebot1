4fdb9a4a76274cc2567ed4b6e6468775e8a96e53
Almortagel <105941198+Almortage@users.noreply.github.com>
2023-09-15 13:44:26 +0300

    Add files via upload

--- AnonX/plugins/keb.py
+++ AnonX/plugins/keb.py
@@ -59,7 +59,7 @@ async def upbkgt(client: Client, message: Message):
     )
 
     @app.on_message(filters.command(["سوره", "قران"], ""))
-async def ihd(client: Client, message: Message):
+async def upbkgt(client: Client, message: Message):
     rl = random.randint(1,90)
     url = f"https://t.me/opuml/{rl}"
     await client.send_voice(message.chat.id,url,caption="🐉 ¦ تـم اختيـار ايـه قرآنيه لـك",parse_mode="html",
@@ -74,7 +74,7 @@ async def ihd(client: Client, message: Message):
     )
 
 @app.on_message(filters.command(["الشيخ", "النقشبندي", "نقشبندي"], ""))
-async def ihd(client: Client, message: Message):
+async def upbkgt(client: Client, message: Message):
     rl = random.randint(1,90)
     url = f"https://t.me/ggcnjj/{rl}"
     await client.send_voice(message.chat.id,url,caption="🥹♥ ¦ تـم اختيـار الشيخ نقشبندي لـك",parse_mode="html",
@@ -89,7 +89,7 @@ async def ihd(client: Client, message: Message):
     )
     
     @app.on_message(filters.command(["عبدالباسط", "عبدالباسط عبدالصمد"], ""))
-async def ihd(client: Client, message: Message):
+async def upbkgt(client: Client, message: Message):
     rl = random.randint(1,50)
     url = f"https://t.me/telawatnader/{rl}"
     await client.send_audio(message.chat.id,url,caption="🥹♥ ¦ تـم اختيـار الشيخ عبدالباسط لـك",parse_mode="html",
