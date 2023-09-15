cba0e82a94941af56c918b55697118398a0e5f82
Almortagel <105941198+Almortage@users.noreply.github.com>
2023-09-15 13:50:22 +0300

    Add files via upload

--- AnonX/plugins/keb.py
+++ AnonX/plugins/keb.py
@@ -58,35 +58,7 @@ async def upbkgt(client: Client, message: Message):
         reply_markup=ReplyKeyboardRemove()
     )
 
-    @app.on_message(command(["سوره", "قران"], ""))
-async def upbkgt(client: Client, message: Message):
-    rl = random.randint(1,90)
-    url = f"https://t.me/opuml/{rl}"
-    await client.send_voice(message.chat.id,url,caption="🐉 ¦ تـم اختيـار ايـه قرآنيه لـك",parse_mode="html",
-    reply_markup=InlineKeyboardMarkup(
-            [
-                [
-                    InlineKeyboardButton(
-                        message.from_user.first_name, url=f"https://t.me/{message.from_user.username}")
-                ],
-            ]
-        )
-    )
 
-@app.on_message(command(["الشيخ", "النقشبندي", "نقشبندي"], ""))
-async def upbkgt(client: Client, message: Message):
-    rl = random.randint(1,90)
-    url = f"https://t.me/ggcnjj/{rl}"
-    await client.send_voice(message.chat.id,url,caption="🥹♥ ¦ تـم اختيـار الشيخ نقشبندي لـك",parse_mode="html",
-    reply_markup=InlineKeyboardMarkup(
-            [
-                [
-                    InlineKeyboardButton(
-                        message.from_user.first_name, url=f"https://t.me/{message.from_user.username}")
-                ],
-            ]
-        )
-    )
     
     @app.on_message(command(["عبدالباسط", "عبدالباسط عبدالصمد"], ""))
 async def upbkgt(client: Client, message: Message):
