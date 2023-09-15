c3d98e008172de8458fde5245c2a117670e5e584
Almortagel <105941198+Almortage@users.noreply.github.com>
2023-09-16 01:25:21 +0300

    Add files via upload

--- AnonX/plugins/kibalmortagel.py
+++ AnonX/plugins/kibalmortagel.py
@@ -13,7 +13,7 @@ from AnonX import app
 from random import  choice, randint
 
 @app.on_message(command(["صوره", "🕷", "صورهه", "صور"]))
-async def upbkgt(client: Client, message: Message):
+async def ihd(client: Client, message: Message):
     rl = random.randint(2,50)
     url = f"https://t.me/vnnkli/{rl}"
     await client.send_photo(message.chat.id,url,caption="🐉 ¦ تـم اختيـار صوره لـك",parse_mode="html",
@@ -29,7 +29,7 @@ async def upbkgt(client: Client, message: Message):
 
 
 @app.on_message(command(["انميي", "انمي"]))
-async def upbkgt(client: Client, message: Message):
+async def ihd(client: Client, message: Message):
     rl = random.randint(3,153)
     url = f"https://t.me/LoreBots7/{rl}"
     await client.send_photo(message.chat.id,url,caption="🐉 ¦ تـم اختيـار انمي لـك",parse_mode="html",
@@ -45,7 +45,7 @@ async def upbkgt(client: Client, message: Message):
 
 
 @app.on_message(command(["متحركه. 🎬", "متحركه"]))
-async def upbkgt(client: Client, message: Message):
+async def ihd(client: Client, message: Message):
     rl = random.randint(2,925)
     url = f"https://t.me/GifWaTaN/{rl}"
     await client.send_animation(message.chat.id,url,caption="🐉 ¦ تـم اختيـار ملصق لـك",parse_mode="html",
@@ -60,7 +60,7 @@ async def upbkgt(client: Client, message: Message):
     )
 
 @app.on_message(command(["اقتباسات", "اقتباس"]))
-async def upbkgt(client: Client, message: Message):
+async def ihd(client: Client, message: Message):
     rl = random.randint(2,102)
     url = f"https://t.me/LoreBots9/{rl}"
     await client.send_photo(message.chat.id,url,caption="🐉 ¦ تـم اختيـار اقتباس لـك",parse_mode="html",
@@ -75,7 +75,7 @@ async def upbkgt(client: Client, message: Message):
     )
 
 @app.on_message(command(["هيدرا", "هيدرات"]))
-async def upbkgt(client: Client, message: Message):
+async def ihd(client: Client, message: Message):
     rl = random.randint(2,152)
     url = f"https://t.me/flflfldld/{rl}"
     await client.send_photo(message.chat.id,url,caption="🐉 ¦ تـم اختيـار هيدرات لـك",parse_mode="html",
@@ -90,7 +90,7 @@ async def upbkgt(client: Client, message: Message):
     )
 
 @app.on_message(command(["صور", "افتار بنات"]))
-async def upbkgt(client: Client, message: Message):
+async def ihd(client: Client, message: Message):
     rl = random.randint(2,215)
     url = f"https://t.me/vvyuol/{rl}"
     await client.send_photo(message.chat.id,url,caption="🐉 ¦ تـم اختيـار صوره لـك",parse_mode="html",
@@ -105,7 +105,7 @@ async def upbkgt(client: Client, message: Message):
     )
 
 @app.on_message(command(["صور شباب", "افتار شباب"]))
-async def upbkgt(client: Client, message: Message):
+async def ihd(client: Client, message: Message):
     rl = random.randint(2,174)
     url = f"https://t.me/vgbmm/{rl}"
     await client.send_photo(message.chat.id,url,caption="🐉 ¦ تـم اختيـار صوره لـك",parse_mode="html",
@@ -119,23 +119,8 @@ async def upbkgt(client: Client, message: Message):
         )
     )
 
-@app.on_message(command(["فيلم", "فيلمك. 🎥"]))
-async def upbkgt(client: Client, message: Message):
-    rl = random.randint(1,34)
-    url = f"https://t.me/gyigkk/{rl}"
-    await client.send_audio(message.chat.id,url,caption="🐉 ¦ تـم اختيـار فيلم لـك",parse_mode="html",
-    reply_markup=InlineKeyboardMarkup(
-            [
-                [
-                    InlineKeyboardButton(
-                        message.from_user.first_name, url=f"https://t.me/{message.from_user.username}")
-                ],
-            ]
-        )
-    )
-
 @app.on_message(command(["استوري", "استوريهات. 🥹"]))
-async def upbkgt(client: Client, message: Message):
+async def ihd(client: Client, message: Message):
     rl = random.randint(2,147)
     url = f"https://t.me/yoipopl/{rl}"
     await client.send_audio(message.chat.id,url,caption="🐉 ¦ تـم اختيـار استوري لـك",parse_mode="html",
@@ -150,7 +135,7 @@ async def upbkgt(client: Client, message: Message):
     )
     
     @app.on_message(command(["سوره", "قران"]))
-async def upbkgt(client: Client, message: Message):
+async def ihd(client: Client, message: Message):
     rl = random.randint(7,276)
     url = f"https://t.me/ALMORTAGELRSK/{rl}"
     await client.send_voice(message.chat.id,url,caption="🐉 ¦ تـم اختيـار ايـه قرآنيه لـك",parse_mode="html",
@@ -165,7 +150,7 @@ async def upbkgt(client: Client, message: Message):
     )
 
 @app.on_message(command(["الشيخ", "النقشبندي", "نقشبندي"]))
-async def upbkgt(client: Client, message: Message):
+async def ihd(client: Client, message: Message):
     rl = random.randint(2,113)
     url = f"https://t.me/ggcnjj/{rl}"
     await client.send_voice(message.chat.id,url,caption="🥹♥ ¦ تـم اختيـار الشيخ نقشبندي لـك",parse_mode="html",
@@ -180,7 +165,7 @@ async def upbkgt(client: Client, message: Message):
     )
     
     @app.on_message(command(["عبدالباسط", "عبدالباسط عبدالصمد"]))
-async def upbkgt(client: Client, message: Message):
+async def ihd(client: Client, message: Message):
     rl = random.randint(7,264)
     url = f"https://t.me/telawatnader/{rl}"
     await client.send_audio(message.chat.id,url,caption="🥹♥ ¦ تـم اختيـار الشيخ عبدالباسط لـك",parse_mode="html",
